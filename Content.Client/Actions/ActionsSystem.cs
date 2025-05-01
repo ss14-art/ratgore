654d93f92c4373f2e6f8cf15e31c8c36c975b81a
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2025-05-01 10:11:35 +1000

    Fix action ent prediction (#37076)
    Buttons don't get created in the predicted methods only on state handler.

--- Content.Client/Actions/ActionsSystem.cs
+++ Content.Client/Actions/ActionsSystem.cs
@@ -204,6 +204,7 @@ namespace Content.Client.Actions
                 return;
 
             OnActionAdded?.Invoke(actionId);
+            ActionsUpdated?.Invoke();
         }
 
         protected override void ActionRemoved(EntityUid performer, EntityUid actionId, ActionsComponent comp, BaseActionComponent action)
@@ -212,6 +213,7 @@ namespace Content.Client.Actions
                 return;
 
             OnActionRemoved?.Invoke(actionId);
+            ActionsUpdated?.Invoke();
         }
 
         public IEnumerable<(EntityUid Id, BaseActionComponent Comp)> GetClientActions()
