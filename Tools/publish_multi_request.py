53548656a2882b45738dfc8e0175974fdfff69e2
AL-S? <99600368+cryals@users.noreply.github.com>
2026-01-21 22:09:05 +0300

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -14,7 +14,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.ss14.art/"
+ROBUST_CDN_URL = "https://cdn.ss14.art"
 FORK_ID = "ratgore14"
 
 def main():
