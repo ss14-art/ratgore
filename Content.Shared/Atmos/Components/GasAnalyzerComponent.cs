39ef71a110c917099b4219ed7e0978c6a42e74b9
SunChan07 <mix96.08.12@gmail.com>
2026-04-07 18:29:22 +0900

    GasAnalyzerComponentFix

--- Content.Shared/Atmos/Components/GasAnalyzerComponent.cs
+++ Content.Shared/Atmos/Components/GasAnalyzerComponent.cs
@@ -1,113 +1,19 @@
-using Robust.Shared.GameStates;
-using Robust.Shared.Map;
-using Robust.Shared.Serialization;
+namespace Content.Server.Atmos.Components;
 
-namespace Content.Shared.Atmos.Components;
-
-[RegisterComponent, NetworkedComponent]
-public sealed partial class GasAnalyzerComponent : Component
+/// <summary>
+/// Used to keep track of which analyzers are active for update purposes
+/// </summary>
+[RegisterComponent]
+public sealed partial class ActiveGasAnalyzerComponent : Component
 {
-    [ViewVariables]
-    public EntityUid? Target;
-
-    [ViewVariables]
-    public EntityUid User;
-
-    [ViewVariables(VVAccess.ReadWrite)]
-    public EntityCoordinates? LastPosition;
-
-    [DataField("enabled"), ViewVariables(VVAccess.ReadWrite)]
-    public bool Enabled;
-
-    [Serializable, NetSerializable]
-    public enum GasAnalyzerUiKey
-    {
-        Key,
-    }
+    // Set to a tiny bit after the default because otherwise the user often gets a blank window when first using
+    [DataField("accumulatedFrameTime"), ViewVariables(VVAccess.ReadWrite)]
+    public float AccumulatedFrametime = 2.01f;
 
     /// <summary>
-    /// Atmospheric data is gathered in the system and sent to the user
+    /// How often to update the analyzer
     /// </summary>
-    [Serializable, NetSerializable]
-    public sealed class GasAnalyzerUserMessage : BoundUserInterfaceMessage
-    {
-        public string DeviceName;
-        public NetEntity DeviceUid;
-        public bool DeviceFlipped;
-        public string? Error;
-        public GasMixEntry[] NodeGasMixes;
-        public GasAnalyzerUserMessage(GasMixEntry[] nodeGasMixes, string deviceName, NetEntity deviceUid, bool deviceFlipped, string? error = null)
-        {
-            NodeGasMixes = nodeGasMixes;
-            DeviceName = deviceName;
-            DeviceUid = deviceUid;
-            DeviceFlipped = deviceFlipped;
-            Error = error;
-        }
-    }
-
-    /// <summary>
-    /// Contains information on a gas mix entry, turns into a tab in the UI
-    /// </summary>
-    [Serializable, NetSerializable]
-    public struct GasMixEntry
-    {
-        /// <summary>
-        /// Name of the tab in the UI
-        /// </summary>
-        public readonly string Name;
-        public readonly float Volume;
-        public readonly float Pressure;
-        public readonly float Temperature;
-        public readonly GasEntry[]? Gases;
-
-        public GasMixEntry(string name, float volume, float pressure, float temperature, GasEntry[]? gases = null)
-        {
-            Name = name;
-            Volume = volume;
-            Pressure = pressure;
-            Temperature = temperature;
-            Gases = gases;
-        }
-    }
-
-    /// <summary>
-    /// Individual gas entry data for populating the UI
-    /// </summary>
-    [Serializable, NetSerializable]
-    public struct GasEntry
-    {
-        public readonly string Name;
-        public readonly float Amount;
-        public readonly string Color;
-
-        public GasEntry(string name, float amount, string color)
-        {
-            Name = name;
-            Amount = amount;
-            Color = color;
-        }
-
-        public override string ToString()
-        {
-            // e.g. "Plasma: 2000 mol"
-            return Loc.GetString(
-                "gas-entry-info",
-                 ("gasName", Name),
-                 ("gasAmount", Amount));
-        }
-    }
-
-    [Serializable, NetSerializable]
-    public sealed class GasAnalyzerDisableMessage : BoundUserInterfaceMessage
-    {
-
-    }
-}
-
-[Serializable, NetSerializable]
-public enum GasAnalyzerVisuals : byte
-{
-    Enabled,
+    [DataField("updateInterval"), ViewVariables(VVAccess.ReadWrite)]
+    public float UpdateInterval = 1f;
 }
 
