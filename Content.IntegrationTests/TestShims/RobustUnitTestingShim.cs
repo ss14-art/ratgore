0d08144b511c4554ad6d41ebce0a7fb59b04e2c8
SunChan07 <mix96.08.12@gmail.com>
2026-04-14 20:07:27 +0900

    DotNetFix.10

---
+++ Content.IntegrationTests/TestShims/RobustUnitTestingShim.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace Robust.UnitTesting
+{
+    public enum UnitTestProject
+    {
+        Shared = 0,
+        Client = 1,
+        Server = 2,
+        Tools  = 3
+    }
+
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class)]
+    public sealed class UnitTestProjectAttribute : Attribute
+    {
+        public UnitTestProjectAttribute() { }
+        public UnitTestProjectAttribute(UnitTestProject project) => Project = project;
+        public UnitTestProject Project { get; } = UnitTestProject.Shared;
+    }
+    public abstract class RobustUnitTest
+    {
+        public virtual UnitTestProject Project => UnitTestProject.Shared;
+        protected virtual void OverrideIoC() { }
+        protected virtual Assembly[] GetContentAssemblies() => Array.Empty<Assembly>();
+    }
+}
