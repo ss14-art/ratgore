4e5bb61bcb6eb2a68eac79afb3157bb27685fc4b
SunChan07 <mix96.08.12@gmail.com>
2026-04-14 20:50:39 +0900

    Aboba...

--- Content.Tests/Server/Jobs/JobQueueTest.cs
+++ Content.Tests/Server/Jobs/JobQueueTest.cs
@@ -6,6 +6,7 @@ using Robust.Shared.CPUJob.JobQueues.Queues;
 using NUnit.Framework;
 using Robust.Shared.Timing;
 using Robust.UnitTesting;
+using NUnit.Framework;
 
 namespace Content.Tests.Server.Jobs
 {
@@ -47,15 +48,15 @@ namespace Content.Tests.Server.Jobs
 
             queue.Process();
             Assert.That(job.Status, Is.EqualTo(JobStatus.Paused));
-            Assert.That((float)job.DebugTime, new ApproxEqualityConstraint(1f));
+            Assert.That(actual1, Is.EqualTo(expected1).Within(tolerance));
             queue.Process();
             Assert.That(job.Status, Is.EqualTo(JobStatus.Paused));
-            Assert.That((float)job.DebugTime, new ApproxEqualityConstraint(2f));
+            Assert.That(actual2, Is.EqualTo(expected2).Within(tolerance));
             queue.Process();
             Assert.That(job.Status, Is.EqualTo(JobStatus.Finished));
 
             Assert.That(job.Result, Is.EqualTo("foo!"));
-            Assert.That((float)job.DebugTime, new ApproxEqualityConstraint(2.4f));
+            Assert.That(actual3, Is.EqualTo(expected3).Within(tolerance));
         }
 
         [Test]
@@ -77,7 +78,7 @@ namespace Content.Tests.Server.Jobs
             cts.Cancel();
             queue.Process();
             Assert.That(job.Status, Is.EqualTo(JobStatus.Finished));
-            Assert.That((float)job.DebugTime, new ApproxEqualityConstraint(2.0f));
+            Assert.That(total,  Is.EqualTo(expectedTotal).Within(tolerance));
 
             Assert.That(job.Result, Is.Null);
         }
